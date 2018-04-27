69cce014a9d35e0959c22c6d3c2b9b2da6a15e84
Unknown <ivanbustos7@gmail.com>
2018-04-27 08:10:31 -0300

    pav
    primera

---
+++ WindowsApplication3/WindowsApplication3/FRM_ABM_3K6_2018.designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FRM_ABM_3K6_2018
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FRM_ABM_3K6_2018))
+        Me.lbl_cantidad = New System.Windows.Forms.Label()
+        Me.grid1 = New System.Windows.Forms.DataGridView()
+        Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.apellido = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.tipo_doc = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.documento = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.id_tipo_documento = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.txt_sexo = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cmb_estado_civil = New System.Windows.Forms.ComboBox()
+        Me.txt_documento = New System.Windows.Forms.MaskedTextBox()
+        Me.txt_nombres = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cmb_tipo_documento = New System.Windows.Forms.ComboBox()
+        Me.txt_apellido = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmd_salir = New System.Windows.Forms.Button()
+        Me.cmd_cancelar = New System.Windows.Forms.Button()
+        Me.cmd_grabar = New System.Windows.Forms.Button()
+        Me.cmd_nuevo = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.chk_borrar = New System.Windows.Forms.CheckBox()
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl_cantidad
+        '
+        Me.lbl_cantidad.AutoSize = True
+        Me.lbl_cantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_cantidad.Location = New System.Drawing.Point(411, 145)
+        Me.lbl_cantidad.Name = "lbl_cantidad"
+        Me.lbl_cantidad.Size = New System.Drawing.Size(16, 16)
+        Me.lbl_cantidad.TabIndex = 50
+        Me.lbl_cantidad.Text = "0"
+        '
+        'grid1
+        '
+        Me.grid1.AllowUserToAddRows = False
+        Me.grid1.AllowUserToDeleteRows = False
+        Me.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grid1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Nombre, Me.apellido, Me.tipo_doc, Me.documento, Me.id_tipo_documento})
+        Me.grid1.Location = New System.Drawing.Point(18, 169)
+        Me.grid1.Name = "grid1"
+        Me.grid1.ReadOnly = True
+        Me.grid1.Size = New System.Drawing.Size(463, 143)
+        Me.grid1.TabIndex = 48
+        '
+        'Nombre
+        '
+        Me.Nombre.HeaderText = "Nombres"
+        Me.Nombre.Name = "Nombre"
+        Me.Nombre.ReadOnly = True
+        Me.Nombre.Width = 150
+        '
+        'apellido
+        '
+        Me.apellido.HeaderText = "Apellido"
+        Me.apellido.Name = "apellido"
+        Me.apellido.ReadOnly = True
+        Me.apellido.Width = 120
+        '
+        'tipo_doc
+        '
+        Me.tipo_doc.HeaderText = "Tipo Doc"
+        Me.tipo_doc.Name = "tipo_doc"
+        Me.tipo_doc.ReadOnly = True
+        Me.tipo_doc.Width = 50
+        '
+        'documento
+        '
+        Me.documento.HeaderText = "Documento"
+        Me.documento.Name = "documento"
+        Me.documento.ReadOnly = True
+        Me.documento.Width = 70
+        '
+        'id_tipo_documento
+        '
+        Me.id_tipo_documento.HeaderText = "id_tipo_documento"
+        Me.id_tipo_documento.Name = "id_tipo_documento"
+        Me.id_tipo_documento.ReadOnly = True
+        Me.id_tipo_documento.Visible = False
+        '
+        'txt_sexo
+        '
+        Me.txt_sexo.Location = New System.Drawing.Point(124, 116)
+        Me.txt_sexo.Mask = "L"
+        Me.txt_sexo.Name = "txt_sexo"
+        Me.txt_sexo.Size = New System.Drawing.Size(16, 20)
+        Me.txt_sexo.TabIndex = 36
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Location = New System.Drawing.Point(74, 119)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 13)
+        Me.Label6.TabIndex = 47
+        Me.Label6.Text = "Sexo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Location = New System.Drawing.Point(49, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 46
+        Me.Label5.Text = "Estado Civil"
+        '
+        'cmb_estado_civil
+        '
+        Me.cmb_estado_civil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmb_estado_civil.FormattingEnabled = True
+        Me.cmb_estado_civil.Location = New System.Drawing.Point(124, 142)
+        Me.cmb_estado_civil.Name = "cmb_estado_civil"
+        Me.cmb_estado_civil.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_estado_civil.TabIndex = 37
+        '
+        'txt_documento
+        '
+        Me.txt_documento.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_documento.Location = New System.Drawing.Point(124, 90)
+        Me.txt_documento.Mask = "99999999"
+        Me.txt_documento.Name = "txt_documento"
+        Me.txt_documento.Size = New System.Drawing.Size(57, 20)
+        Me.txt_documento.TabIndex = 35
+        '
+        'txt_nombres
+        '
+        Me.txt_nombres.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_nombres.Location = New System.Drawing.Point(124, 34)
+        Me.txt_nombres.Name = "txt_nombres"
+        Me.txt_nombres.Size = New System.Drawing.Size(337, 20)
+        Me.txt_nombres.TabIndex = 33
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Location = New System.Drawing.Point(62, 38)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 45
+        Me.Label4.Text = "Nombres"
+        '
+        'cmb_tipo_documento
+        '
+        Me.cmb_tipo_documento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmb_tipo_documento.FormattingEnabled = True
+        Me.cmb_tipo_documento.Location = New System.Drawing.Point(124, 63)
+        Me.cmb_tipo_documento.Name = "cmb_tipo_documento"
+        Me.cmb_tipo_documento.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_tipo_documento.TabIndex = 34
+        '
+        'txt_apellido
+        '
+        Me.txt_apellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_apellido.Location = New System.Drawing.Point(124, 12)
+        Me.txt_apellido.Name = "txt_apellido"
+        Me.txt_apellido.Size = New System.Drawing.Size(337, 20)
+        Me.txt_apellido.TabIndex = 32
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Location = New System.Drawing.Point(49, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 13)
+        Me.Label3.TabIndex = 44
+        Me.Label3.Text = "Documento"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(25, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 43
+        Me.Label2.Text = "Tipo Documento"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(67, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 42
+        Me.Label1.Text = "Apellido"
+        '
+        'cmd_salir
+        '
+        Me.cmd_salir.Image = CType(resources.GetObject("cmd_salir.Image"), System.Drawing.Image)
+        Me.cmd_salir.Location = New System.Drawing.Point(447, 318)
+        Me.cmd_salir.Name = "cmd_salir"
+        Me.cmd_salir.Size = New System.Drawing.Size(40, 44)
+        Me.cmd_salir.TabIndex = 41
+        Me.cmd_salir.UseVisualStyleBackColor = True
+        '
+        'cmd_cancelar
+        '
+        Me.cmd_cancelar.Image = CType(resources.GetObject("cmd_cancelar.Image"), System.Drawing.Image)
+        Me.cmd_cancelar.Location = New System.Drawing.Point(114, 318)
+        Me.cmd_cancelar.Name = "cmd_cancelar"
+        Me.cmd_cancelar.Size = New System.Drawing.Size(40, 44)
+        Me.cmd_cancelar.TabIndex = 40
+        Me.cmd_cancelar.UseVisualStyleBackColor = True
+        '
+        'cmd_grabar
+        '
+        Me.cmd_grabar.Image = CType(resources.GetObject("cmd_grabar.Image"), System.Drawing.Image)
+        Me.cmd_grabar.Location = New System.Drawing.Point(68, 318)
+        Me.cmd_grabar.Name = "cmd_grabar"
+        Me.cmd_grabar.Size = New System.Drawing.Size(40, 44)
+        Me.cmd_grabar.TabIndex = 39
+        Me.cmd_grabar.UseVisualStyleBackColor = True
+        '
+        'cmd_nuevo
+        '
+        Me.cmd_nuevo.Image = CType(resources.GetObject("cmd_nuevo.Image"), System.Drawing.Image)
+        Me.cmd_nuevo.Location = New System.Drawing.Point(22, 318)
+        Me.cmd_nuevo.Name = "cmd_nuevo"
+        Me.cmd_nuevo.Size = New System.Drawing.Size(40, 44)
+        Me.cmd_nuevo.TabIndex = 38
+        Me.ToolTip1.SetToolTip(Me.cmd_nuevo, "Nuevo registro")
+        Me.cmd_nuevo.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'chk_borrar
+        '
+        Me.chk_borrar.AutoSize = True
+        Me.chk_borrar.Location = New System.Drawing.Point(237, 318)
+        Me.chk_borrar.Name = "chk_borrar"
+        Me.chk_borrar.Size = New System.Drawing.Size(96, 17)
+        Me.chk_borrar.TabIndex = 51
+        Me.chk_borrar.Text = "Borrar Registro"
+        Me.chk_borrar.UseVisualStyleBackColor = True
+        '
+        'FRM_ABM_3K6_2018
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 371)
+        Me.Controls.Add(Me.chk_borrar)
+        Me.Controls.Add(Me.lbl_cantidad)
+        Me.Controls.Add(Me.grid1)
+        Me.Controls.Add(Me.cmd_salir)
+        Me.Controls.Add(Me.cmd_cancelar)
+        Me.Controls.Add(Me.cmd_grabar)
+        Me.Controls.Add(Me.cmd_nuevo)
+        Me.Controls.Add(Me.txt_sexo)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cmb_estado_civil)
+        Me.Controls.Add(Me.txt_documento)
+        Me.Controls.Add(Me.txt_nombres)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cmb_tipo_documento)
+        Me.Controls.Add(Me.txt_apellido)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FRM_ABM_3K6_2018"
+        Me.Text = "FRM_ABM_3K6_2018"
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_cantidad As System.Windows.Forms.Label
+    Friend WithEvents grid1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Nombre As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents apellido As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents tipo_doc As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents documento As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents id_tipo_documento As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents cmd_salir As System.Windows.Forms.Button
+    Friend WithEvents cmd_cancelar As System.Windows.Forms.Button
+    Friend WithEvents cmd_grabar As System.Windows.Forms.Button
+    Friend WithEvents cmd_nuevo As System.Windows.Forms.Button
+    Friend WithEvents txt_sexo As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents cmb_estado_civil As System.Windows.Forms.ComboBox
+    Friend WithEvents txt_documento As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents txt_nombres As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cmb_tipo_documento As System.Windows.Forms.ComboBox
+    Friend WithEvents txt_apellido As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents chk_borrar As System.Windows.Forms.CheckBox
+End Class
---
+++ WindowsApplication3/WindowsApplication3/FRM_ABM_3K6_2018.vb
@@ -0,0 +1,233 @@
+﻿Public Class FRM_ABM_3K6_2018
+    Enum tipo_accion
+        alta
+        modificacion
+    End Enum
+    Enum estado_validacion
+        _correcta
+        _error
+    End Enum
+    Enum estado_busqueda
+        encotre
+        no_encontre
+    End Enum
+    Dim accion As tipo_accion = tipo_accion.alta
+    Dim cadena_conexion As String = "Provider=SQLNCLI11;Data Source=maquis;User ID=avisuales1;Initial Catalog=_BD_PERSONAS;password=avisuales1"
+    'Dim cadena_conexion As String = "Provider=SQLOLEDB;Data Source=LENOVO-510-LUIS\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=_BD_PERSONAS;password=avisuales1"
+    Private Function leo_tabla(ByVal nombre_tabla As String) As DataTable
+        Return ejecutar_sql("select * from " & nombre_tabla)
+    End Function
+    Private Function ejecutar_sql(ByVal comando_sql As String) As DataTable
+        Dim conexion As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        Dim tabla As New DataTable
+        conexion.ConnectionString = cadena_conexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = comando_sql
+        If comando_sql.ToUpper.IndexOf("SELECT") = -1 Then
+            cmd.ExecuteNonQuery()
+        Else
+            tabla.Load(cmd.ExecuteReader())
+        End If
+
+        conexion.Close()
+        Return tabla
+    End Function
+
+    Private Sub cargar_grilla()
+        Dim sql As String = ""
+        Dim tabla As New DataTable
+
+        sql &= "SELECT nombre, apellido, n_tipo_documento, nro_documento "
+        sql &= ", p.id_tipo_documento "
+        sql &= "FROM personas p join tipos_documento tp "
+        sql &= "              ON p.id_tipo_documento = tp.id_tipo_documento "
+
+        tabla = ejecutar_sql(sql)
+
+        Dim c As Integer
+        Me.grid1.Rows.Clear()
+
+        For c = 0 To tabla.Rows.Count - 1
+            Me.grid1.Rows.Add(tabla.Rows(c)(0), tabla.Rows(c)(1) _
+                              , tabla.Rows(c)(2), tabla.Rows(c)(3) _
+                              , tabla.Rows(c)(4))
+        Next
+
+    End Sub
+
+    Private Sub cargar_combo(ByRef combo As ComboBox, ByRef tabla As DataTable _
+                             , pk As String, descripcion As String)
+
+        combo.DataSource = tabla
+        combo.DisplayMember = descripcion
+        combo.ValueMember = pk
+
+    End Sub
+
+    Private Sub FRM_ABM_3K6_2018_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cargar_combo(cmb_tipo_documento, leo_tabla("tipos_documento"), "id_tipo_documento", "n_tipo_documento")
+        cargar_combo(cmb_estado_civil, leo_tabla("tipos_estado_civil"), "id_estado_civil", "n_estado_civil")
+        cargar_grilla()
+    End Sub
+
+    Private Sub cmd_nuevo_Click(sender As Object, e As EventArgs) Handles cmd_nuevo.Click
+        Me.accion = tipo_accion.alta
+        Dim txt As Object
+        For Each txt In Me.Controls
+            If txt.GetType.Name = "TextBox" Or txt.GetType.Name = "MaskedTextBox" Then
+                txt.Text = ""
+            End If
+        Next
+        Me.cmb_tipo_documento.SelectedIndex = -1
+        Me.txt_apellido.Focus()
+
+    End Sub
+
+    Private Sub cmd_grabar_Click(sender As Object, e As EventArgs) Handles cmd_grabar.Click
+        ' VALIDAR DATOS VACIOS
+        If Me.validacion() = estado_validacion._correcta Then
+            If accion = tipo_accion.alta Then
+                ' VALIDAR EXISTENCIA DE DATOS 
+                If buscar_persona() = estado_busqueda.encotre Then
+                    MsgBox("Esta Persona ya existe en BD")
+                    Exit Sub
+                Else
+                    'ACCION = ALTA ==> INSERT DE SQL
+                    Me.insertar_registro()
+                End If
+            Else
+                ' ACCION = MODIFICAR ==> UPDATE DE SQL
+                Me.modificar_registro()
+            End If
+        End If
+
+        ' ENVIAR UN MENSAJE AL OPERADOR QUE SE GRABO CORRECTAMENTE
+        MsgBox("Se grabó correctamente", MsgBoxStyle.Exclamation, "Mensaje")
+
+        ' RECARGAR LA GRILLA
+        Me.cargar_grilla()
+        ' ACCION <= MODIFICAR
+    End Sub
+    Private Sub modificar_registro()
+        Dim sql_modificar As String = ""
+        sql_modificar = "UPDATE personas "
+        sql_modificar &= "SET apellido = '" & Me.txt_apellido.Text.Trim & "'"
+        sql_modificar &= ", nombre = '" & Me.txt_nombres.Text.Trim & "'"
+        sql_modificar &= ", sexo = '" & Me.txt_sexo.Text.Trim & "'"
+        sql_modificar &= ", id_estado_civil = " & Me.cmb_estado_civil.SelectedValue
+        sql_modificar &= " WHERE nro_documento = " & Me.txt_documento.Text.Trim
+        sql_modificar &= " AND id_tipo_documento = " & Me.cmb_tipo_documento.SelectedValue
+
+        Me.ejecutar_sql(sql_modificar)
+
+    End Sub
+    Private Sub insertar_registro()
+        Dim sql_insertar As String = ""
+        sql_insertar = "INSERT INTO personas ("
+        sql_insertar &= "nro_documento"
+        sql_insertar &= ", id_tipo_documento"
+        sql_insertar &= ", apellido, nombre, sexo "
+        sql_insertar &= ", id_estado_civil) VALUES ("
+        sql_insertar &= Me.txt_documento.Text.Trim
+        sql_insertar &= ", " & Me.cmb_tipo_documento.SelectedValue
+        sql_insertar &= ", '" & Me.txt_apellido.Text.Trim & "'"
+        sql_insertar &= ", '" & Me.txt_nombres.Text.Trim & "'"
+        sql_insertar &= ", '" & Me.txt_sexo.Text.Trim & "'"
+        sql_insertar &= ", " & Me.cmb_estado_civil.SelectedValue & ")"
+
+        Me.ejecutar_sql(sql_insertar)
+
+    End Sub
+    Private Function buscar_persona() As estado_busqueda
+        Dim sql As String = ""
+        sql = "SELECT * FROM personas "
+        sql &= " WHERE nro_documento = " & Me.txt_documento.Text.Trim
+        sql &= " AND id_tipo_documento = " & Me.cmb_tipo_documento.SelectedValue
+        Dim tabla As New DataTable
+        tabla = Me.ejecutar_sql(sql)
+        If tabla.Rows.Count = 0 Then
+            Return estado_busqueda.no_encontre
+        Else
+            Return estado_busqueda.encotre
+        End If
+    End Function
+    Private Function validacion() As estado_validacion
+        If Me.txt_apellido.Text = "" Then
+            MsgBox("El apellido esta vacio", MsgBoxStyle.Critical, "Error de valicion")
+            Me.txt_apellido.Focus()
+            Return estado_validacion._error
+        End If
+        If Me.txt_nombres.Text = "" Then
+            MsgBox("El nombre esta vacio", MsgBoxStyle.Critical, "Error de valicion")
+            Me.txt_nombres.Focus()
+            Return estado_validacion._error
+        End If
+        If Me.txt_documento.Text = "" Then
+            MsgBox("El documento esta vacio", MsgBoxStyle.Critical, "Error de valicion")
+            Me.txt_documento.Focus()
+            Return estado_validacion._error
+        End If
+        If Me.txt_sexo.Text = "" Then
+            MsgBox("El sexo esta vacio", MsgBoxStyle.Critical, "Error de valicion")
+            Me.txt_sexo.Focus()
+            Return estado_validacion._error
+        End If
+        If Me.cmb_tipo_documento.SelectedIndex = -1 Then
+            MsgBox("El tipo de documento esta vacio", MsgBoxStyle.Critical, "Error de valicion")
+            Me.cmb_tipo_documento.Focus()
+            Return estado_validacion._error
+        End If
+        Return estado_validacion._correcta
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim sql As String = "select count(*) from personas"
+        Dim tabla As DataTable
+        tabla = Me.ejecutar_sql(sql)
+        Me.lbl_cantidad.Text = tabla.Rows(0)(0)
+    End Sub
+
+    Private Sub grid1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles grid1.CellDoubleClick
+        Dim sql As String = ""
+        sql = "SELECT * FROM personas "
+        sql &= " WHERE nro_documento = " & Me.grid1.CurrentRow.Cells(3).Value
+        sql &= " AND id_tipo_documento = " & Me.grid1.CurrentRow.Cells(4).Value
+
+        Dim tabla As New DataTable
+        tabla = Me.ejecutar_sql(sql)
+        If Me._chk_borrar.Checked = True Then
+            If MessageBox.Show("Está seguro que desea borrar a: " & Chr(13) _
+                             & tabla.Rows(0)("apellido").ToString.Trim & ", " _
+                             & tabla.Rows(0)("nombre").ToString.Trim & " " _
+                             & Chr(13) & "cuyo documento es: " _
+                             & tabla.Rows(0)("nro_documento"), "Importante", MessageBoxButtons.YesNo _
+                             , MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+                Dim sql_borrar As String = ""
+                sql_borrar = "DELETE FROM personas "
+                sql_borrar &= " WHERE nro_documento = " & Me.grid1.CurrentRow.Cells(3).Value
+                sql_borrar &= " AND id_tipo_documento = " & Me.grid1.CurrentRow.Cells(4).Value
+                Me.ejecutar_sql(sql_borrar)
+                Me.cargar_grilla()
+            End If
+            Exit Sub
+        End If
+        If tabla.Rows.Count = 1 Then
+            Me.txt_apellido.Text = tabla.Rows(0)("apellido")
+            Me.txt_nombres.Text = tabla.Rows(0)("nombre")
+            Me.txt_documento.Text = tabla.Rows(0)("nro_documento")
+            Me.txt_sexo.Text = tabla.Rows(0)("sexo")
+            Me.cmb_tipo_documento.SelectedValue = tabla.Rows(0)("id_tipo_documento")
+            Me.cmb_estado_civil.SelectedValue = tabla.Rows(0)("id_estado_civil")
+            Me.txt_documento.Enabled = False
+            Me.cmb_tipo_documento.Enabled = False
+            Me.accion = tipo_accion.modificacion
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles chk_borrar.CheckedChanged
+
+    End Sub
+End Class
